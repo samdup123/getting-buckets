03fea01432259e50c52b6d01e62cad5fffc0d078
sam <samdup123@gmail.com>
2018-11-07 17:52:44 -0500

    only drops a limited number of balls

--- love/main.lua
+++ love/main.lua
@@ -11,15 +11,32 @@ function love.load()
     chutes = Chutes(num_chutes, length_of_chutes)
     ball_dropper = Ball_Dropper(num_chutes)
 
-
     width_of_chute = 50
     distance_unit = width_of_chute
     chute_rect = {start_x = 10, start_y = 10, width = width_of_chute * num_chutes, height = distance_unit * length_of_chutes}
     ball = {radius = width_of_chute / 2}
 
+    bucket_location = 1
+    balls_that_fell_in_trough = 0
+
+    number_of_balls_that_will_fall = 30
+    fallen_balls = 0
+
     function tock()
-        local new_ball = ball_dropper.chutes_in_which_balls_are_dropped()
-        chutes.tock({new_ball})
+        print('tocking')
+        local new_ball = {}
+        if fallen_balls ~= number_of_balls_that_will_fall then
+            print('inserting a new ball')
+            table.insert(new_ball, ball_dropper.chutes_in_which_balls_are_dropped())
+            fallen_balls = fallen_balls + 1
+        end
+        chutes.tock(new_ball)
+    end
+
+    function ball_exits(chute_num)
+        if not chute_num == bucket_location then
+            balls_that_fell_in_trough = balls_that_fell_in_trough + 1
+        end
     end
 
     time = 0
@@ -27,7 +44,7 @@ end
 
 function love.update(dt)
     time = time + dt
-    if time > .1 then
+    if time > .4 then
         time = 0
         tock()
     end
