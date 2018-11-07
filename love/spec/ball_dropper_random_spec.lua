4887d0efa976bc501036fa80220e918739806fec
sam <samdup123@gmail.com>
2018-11-07 18:07:20 -0500

    made the ball dropper have a set count of balls it drops

--- love/spec/ball_dropper_random_spec.lua
+++ love/spec/ball_dropper_random_spec.lua
@@ -4,16 +4,24 @@ describe('random ball dropper', function()
 
     it('should drop a random ball every time', function()
         local number_of_chutes = 10
-        local dropper = Dropper(number_of_chutes)
+        local number_of_balls_to_drop = 30
+        local dropper = Dropper(number_of_chutes, number_of_balls_to_drop)
 
         for i = 1, 10 do
-            local chutes_in_which_balls_are_dropped = dropper.chutes_in_which_balls_are_dropped()
+            local chutes_in_which_balls_are_dropped = dropper.tock()
             assert.is_true(1 <= chutes_in_which_balls_are_dropped and chutes_in_which_balls_are_dropped <= 10)
         end
     end)
 
     it('should implement tock method but that should do nothing', function()
-        local dropper = Dropper(number_of_chutes)
-        dropper.tock()
+        local number_of_chutes = 10
+        local number_of_balls_to_drop = 30
+        local dropper = Dropper(number_of_chutes, number_of_balls_to_drop)
+
+        for i = 1, number_of_balls_to_drop do
+            dropper.tock()
+        end
+
+        assert.is.same({}, dropper.tock())
     end)
 end)
--- love/src/ball_dropper_random.lua
+++ love/src/ball_dropper_random.lua
@@ -1,10 +1,15 @@
-return function(number_of_chutes)
-
+return function(number_of_chutes, number_of_balls_to_drop)
+    local num_dropped = 0
     return {
-        chutes_in_which_balls_are_dropped = function()
-            return math.random(number_of_chutes)
-        end,
-        tock = function() end
+        tock = function()
+            if num_dropped < number_of_balls_to_drop then
+                local chute_to_drop_ball = math.random(number_of_chutes)
+                num_dropped = num_dropped + 1
+                return chute_to_drop_ball
+            else
+                return {}
+            end
+        end
     }
 end
     
