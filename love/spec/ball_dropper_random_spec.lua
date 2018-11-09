496145e60dd79503656951b32d338f9e52fb49b5
sam <samdup123@gmail.com>
2018-11-09 09:23:44 -0500

    random ball dropper drops every few tocks

--- love/spec/ball_dropper_random_spec.lua
+++ love/spec/ball_dropper_random_spec.lua
@@ -2,21 +2,29 @@ describe('random ball dropper', function()
     math.randomseed(os.time())
     local Dropper = require'ball_dropper_random'
 
-    it('should drop a random ball every time', function()
+    it('should drop a random ball every few tocks', function()
         local number_of_chutes = 10
         local number_of_balls_to_drop = 30
-        local dropper = Dropper(number_of_chutes, number_of_balls_to_drop)
+        local tocks_between_drops = 4
+        local dropper = Dropper(number_of_chutes, number_of_balls_to_drop, tocks_between_drops)
+        
+        
+        for i = 1, (number_of_balls_to_drop // 2) do
 
-        for i = 1, 10 do
-            local chutes_in_which_balls_are_dropped = dropper.tock()
-            assert.is_true(1 <= chutes_in_which_balls_are_dropped and chutes_in_which_balls_are_dropped <= 10)
+            local chute_in_which_ball_is_dropped = dropper.tock()
+            assert.is_true(1 <= chute_in_which_ball_is_dropped and chute_in_which_ball_is_dropped <= number_of_chutes)
+
+            for j = 1,tocks_between_drops do
+                assert.is.same({}, dropper.tock())
+            end
         end
     end)
 
     it('should stop dropping balls after a time', function()
         local number_of_chutes = 10
         local number_of_balls_to_drop = 30
-        local dropper = Dropper(number_of_chutes, number_of_balls_to_drop)
+        local tocks_between_drops = 0
+        local dropper = Dropper(number_of_chutes, number_of_balls_to_drop, tocks_between_drops)
 
         for i = 1, number_of_balls_to_drop do
             assert.is_false(dropper.done())
@@ -24,6 +32,8 @@ describe('random ball dropper', function()
         end
 
         assert.is_true(dropper.done())
-        assert.is.same({}, dropper.tock())
+        for i = 1,10 do
+            assert.is.same({}, dropper.tock())
+        end
     end)
 end)
--- love/src/ball_dropper_random.lua
+++ love/src/ball_dropper_random.lua
@@ -1,9 +1,18 @@
-return function(number_of_chutes, number_of_balls_to_drop)
+return function(number_of_chutes, number_of_balls_to_drop, tocks_between_drops)
     local num_dropped = 0
     local done_dropping = false
+    local tocks_since_last_drop = tocks_between_drops
+
     return {
         tock = function()
-            if not done_dropping then
+            
+            local enough_tocks_passed = tocks_since_last_drop == tocks_between_drops
+            local still_dropping = not done_dropping
+
+            if still_dropping and enough_tocks_passed then
+
+                tocks_since_last_drop = 0
+
                 local chute_to_drop_ball = math.random(number_of_chutes)
 
                 num_dropped = num_dropped + 1
@@ -11,9 +20,15 @@ return function(number_of_chutes, number_of_balls_to_drop)
                 if num_dropped == number_of_balls_to_drop then 
                     done_dropping = true
                 end
-                
+
                 return chute_to_drop_ball
-            else
+
+            elseif still_dropping and not enough_tocks_passed then
+
+                tocks_since_last_drop = tocks_since_last_drop + 1
+                return {}
+
+            elseif not still_dropping then
                 done_dropping = true
                 return {}
             end
