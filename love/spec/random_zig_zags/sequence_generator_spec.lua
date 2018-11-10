bcddc361d9f3750b9a222d02afcf0e9a53ab5682
sam <samdup123@gmail.com>
2018-11-10 14:47:00 -0500

    added some stuff to the zig zag sequencer

--- love/spec/random_zig_zags/sequence_generator_spec.lua
+++ love/spec/random_zig_zags/sequence_generator_spec.lua
@@ -4,7 +4,7 @@ describe('zig zag sequence generator', function()
     it('should create a simple zig zag from the starting edge going away from the edge', function()
         local total_number_of_chutes, number_of_balls_to_drop = 10, 10
         local first_direction = 1
-        local starting_chute, number_of_direction_changes = 1, 3
+        local starting_chute = 1
         local direction_change_points = {3, 5, 7}
 
         local actual_gantt = generate(
@@ -12,7 +12,6 @@ describe('zig zag sequence generator', function()
             number_of_balls_to_drop,
             starting_chute,
             first_direction,
-            number_of_direction_changes,
             direction_change_points
         )
 
@@ -26,8 +25,8 @@ describe('zig zag sequence generator', function()
 
     it('should create a simple zig zag from the starting edge going into the edge (the direction should simply be switched to away)', function()
         local total_number_of_chutes, number_of_balls_to_drop = 10, 10
-        local first_direction = -   1
-        local starting_chute, number_of_direction_changes = 1, 3
+        local first_direction = -1
+        local starting_chute = 1
         local direction_change_points = {3, 5, 7}
 
         local actual_gantt = generate(
@@ -35,13 +34,56 @@ describe('zig zag sequence generator', function()
             number_of_balls_to_drop,
             starting_chute,
             first_direction,
-            number_of_direction_changes,
             direction_change_points
         )
 
         local expected_gantt = {
             1, 2, 3, '', 3, 2, '', 2, 3, '', 3, 2, 1, ''
         --  1, 2, 3,     4, 5,     6, 7      8, 9, 10
+        }
+
+        assert.are.same(expected_gantt, actual_gantt)
+    end)
+
+    it('should create a zig zag that has no direction changes', function()
+        local total_number_of_chutes, number_of_balls_to_drop = 10, 6
+        local first_direction = 1
+        local starting_chute = 3
+        local direction_change_points = nil
+
+        local actual_gantt = generate(
+            total_number_of_chutes,
+            number_of_balls_to_drop,
+            starting_chute,
+            first_direction,
+            direction_change_points
+        )
+
+        local expected_gantt = {
+            3, 4, 5, 6, 7, 8, ''
+        --  1, 2, 3, 4, 5, 6
+        }
+
+        assert.are.same(expected_gantt, actual_gantt)
+    end)
+
+    it('should create a zig zag with one direction changes', function()
+        local total_number_of_chutes, number_of_balls_to_drop = 10, 6
+        local first_direction = 1
+        local starting_chute = 3
+        local direction_change_points = {4}
+
+        local actual_gantt = generate(
+            total_number_of_chutes,
+            number_of_balls_to_drop,
+            starting_chute,
+            first_direction,
+            direction_change_points
+        )
+
+        local expected_gantt = {
+            3, 4, 5, 6, '', 6, 5, ''
+        --  1, 2, 3, 4,     5, 6
         }
 
         assert.are.same(expected_gantt, actual_gantt)
--- love/src/random_zig_zags/sequence_generator.lua
+++ love/src/random_zig_zags/sequence_generator.lua
@@ -1,16 +1,12 @@
-return function(total_number_of_chutes, number_of_balls_to_drop, starting_chute, first_direction, number_of_direction_changes, direction_change_points)
-    
+
+
+return function(total_number_of_chutes, number_of_balls_to_drop, starting_chute, first_direction, direction_change_points)
+    direction_change_points = direction_change_points or {}
+
     local balls_dropped = 0
     
     local gantt = {}
 
-    local running_change_points = {direction_change_points[1]}
-    for i = 2, #direction_change_points do
-        table.insert(running_change_points, direction_change_points[i] - direction_change_points[i-1])
-    end
-    table.insert(running_change_points, number_of_balls_to_drop - direction_change_points[#direction_change_points])
-   
-    local last_spot_dropped = starting_chute
     local step do
         if starting_chute == 1 then
             step = 1
@@ -20,6 +16,19 @@ return function(total_number_of_chutes, number_of_balls_to_drop, starting_chute,
             step = first_direction
         end
     end
+    local last_spot_dropped = starting_chute
+
+    local running_change_points
+    if #direction_change_points > 0 then
+        running_change_points = {direction_change_points[1]}
+        for i = 2, #direction_change_points do
+            table.insert(running_change_points, direction_change_points[i] - direction_change_points[i-1])
+        end
+        table.insert(running_change_points, number_of_balls_to_drop - direction_change_points[#direction_change_points])
+    else
+        running_change_points = {number_of_balls_to_drop}
+    end
+
     for _,run in ipairs(running_change_points) do
         local start = last_spot_dropped
         local finish = start + (step*(run-step))
