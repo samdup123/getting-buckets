746552c069cbe30ca4700657de93d93c3c19a4b9
sam <samdup123@gmail.com>
2018-11-17 15:05:11 -0500

    added more tests to game history parser

--- love/spec/game_history_parser_spec.lua
+++ love/spec/game_history_parser_spec.lua
@@ -76,5 +76,89 @@ describe('game history parser', function()
         assert.are.same(partial_game_history, parser(game_history, 2))
     end)
 
+    it('should work (again)', function()
+        local partial_game_history = {
+            {
+                bucket_position = 5,
+                balls_in_play = {
+                    {chute = 1, location = 2},
+                    {chute = 3, location = 1},
+                }
+            },
+            {
+                bucket_position = 6,
+                balls_in_play = {
+                    {chute = 1, location = 3},
+                    {chute = 3, location = 2},
+                }
+            },
+            {
+                bucket_position = 7,
+                balls_in_play = {
+                    {chute = 1, location = 4},
+                    {chute = 3, location = 3},
+                },
+            },
+            {
+                bucket_position = 8,
+                balls_in_play = {
+                    {chute = 3, location = 4},
+                },
+                lost_balls = {1}
+            },
+            {
+                bucket_position = 9,
+                balls_in_play = {},
+                lost_balls = {3}
+            }
+        }
 
+        assert.are.same(partial_game_history, parser(game_history, 3))
+    end)
+
+    it('should work with a very large memory length', function()
+        local partial_game_history = {
+            {
+                bucket_position = 4,
+                balls_in_play = {
+                    {chute = 1, location = 1},
+                }
+            },
+            {
+                bucket_position = 5,
+                balls_in_play = {
+                    {chute = 1, location = 2},
+                    {chute = 3, location = 1},
+                }
+            },
+            {
+                bucket_position = 6,
+                balls_in_play = {
+                    {chute = 1, location = 3},
+                    {chute = 3, location = 2},
+                }
+            },
+            {
+                bucket_position = 7,
+                balls_in_play = {
+                    {chute = 1, location = 4},
+                    {chute = 3, location = 3},
+                },
+            },
+            {
+                bucket_position = 8,
+                balls_in_play = {
+                    {chute = 3, location = 4},
+                },
+                lost_balls = {1}
+            },
+            {
+                bucket_position = 9,
+                balls_in_play = {},
+                lost_balls = {3}
+            }
+        }
+
+        assert.are.same(partial_game_history, parser(game_history, 8))
+    end)
 end)
