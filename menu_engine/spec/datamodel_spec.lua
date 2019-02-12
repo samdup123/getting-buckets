25a9ce30dab1ba66969011a876b6d014709fa655
sam <samdup123@gmail.com>
2019-02-12 10:25:56 -0500

    add on change event to datamodel

--- menu_engine/spec/datamodel_spec.lua
+++ menu_engine/spec/datamodel_spec.lua
@@ -1,11 +1,24 @@
 describe('datamodel', function()
     local Datamodel = require'datamodel'
+    local mach = require'mach'
+    local event_subscription_callback = mach.mock_function('callback')
 
     it('should allow for storage and retrieval of information from a label', function()
         local datamodel = Datamodel({'data'})
         assert.is_nil(datamodel.read('data'))
         datamodel.write('data', 4)
         assert.are.equal(datamodel.read('data'), 4)
+    end)
+
+    it('should allow for default data', function()
+        local datamodel = Datamodel({{'data', 0}})
+        assert.are.equal(0, datamodel.read('data'))
+    end)
+
+    it('should allow for some labels with default data and some without', function()
+        local datamodel = Datamodel({{'data', 0}, 'otherdata'})
+        assert.are.equal(0, datamodel.read('data'))
+        assert.is_nil(datamodel.read('otherdata'))
     end)
 
     it('should not allow for retrieval of an unspecified label', function()
@@ -16,5 +29,12 @@ describe('datamodel', function()
     it('should not allow for storage of an unspecified label', function()
         local datamodel = Datamodel()
         assert.has_error(function() datamodel.write('data', 4) end, 'label "data" is not specified')
+    end)
+
+    it('should produce an on-change event when data is changed', function()
+        local datamodel = Datamodel({'data'})
+        datamodel.subscribe_to_on_change(event_subscription_callback)
+        event_subscription_callback:should_be_called_with('data', 4):when(
+            function() datamodel.write('data', 4) end)
     end)
 end)
--- menu_engine/src/datamodel.lua
+++ menu_engine/src/datamodel.lua
@@ -1,10 +1,20 @@
-return function(labels)
-    labels = labels or {}
+return function(items)
+    items = items or {}
     local label_map = {}
-
-    for _,label in ipairs(labels) do label_map[label] = true end
-
     local model = {}
+
+    for _,item in ipairs(items) do
+        if type(item) == 'string' then
+            label_map[item] = true
+        elseif type(item) == 'table' then
+            local label = item[1]
+            local default = item[2]
+            label_map[label] = true
+            model[label] = default
+        end
+    end
+
+    local callback = function() end
 
     return {
         read = function(label)
@@ -12,8 +22,12 @@ return function(labels)
             return model[label]
         end,
         write = function(label, data)
-            if not label_map[label] then error('label "' .. label .. '" is not specified') end
+            if not label_map[label] then error('label "' .. label .. '" is not specified') end  
             model[label] = data
+            callback(label, data)
+        end,
+        subscribe_to_on_change = function(cb)
+            callback = cb
         end
     }
 end
