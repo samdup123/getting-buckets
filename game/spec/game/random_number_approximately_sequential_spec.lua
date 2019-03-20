4f1a72260d58372517bea41e1dc757d5269c1ead
sam <samdup123@gmail.com>
2019-03-20 15:37:15 -0400

    removed : from mach calls

--- game/spec/game/random_number_approximately_sequential_spec.lua
+++ game/spec/game/random_number_approximately_sequential_spec.lua
@@ -12,28 +12,28 @@ describe('random number approximately sequential', function()
         new_val = rand()
         assert.are.equal(11, new_val)
 
-        f:should_be_called_with(6, 8):and_will_return(7):when(function() new_val = rand() end)
+        f.should_be_called_with(6, 8).and_will_return(7).when(function() new_val = rand() end)
         assert.are.equal(18, new_val)
 
-        f:should_be_called_with(6, 8):and_will_return(8):when(function() new_val = rand() end)
+        f.should_be_called_with(6, 8).and_will_return(8).when(function() new_val = rand() end)
         assert.are.equal(14, new_val)
 
-        f:should_be_called_with(6, 8):and_will_return(6):when(function() new_val = rand() end)
+        f.should_be_called_with(6, 8).and_will_return(6).when(function() new_val = rand() end)
         assert.are.equal(8, new_val)
 
-        f:should_be_called_with(6, 8):and_will_return(6):when(function() new_val = rand() end)
+        f.should_be_called_with(6, 8).and_will_return(6).when(function() new_val = rand() end)
         assert.are.equal(2, new_val)
 
-        f:should_be_called_with(6, 8):and_will_return(6):when(function() new_val = rand() end)
+        f.should_be_called_with(6, 8).and_will_return(6).when(function() new_val = rand() end)
         assert.are.equal(6, new_val)
 
-        f:should_be_called_with(6, 8):and_will_return(6):when(function() new_val = rand() end)
+        f.should_be_called_with(6, 8).and_will_return(6).when(function() new_val = rand() end)
         assert.are.equal(12, new_val)
 
-        f:should_be_called_with(6, 8):and_will_return(7):when(function() new_val = rand() end)
+        f.should_be_called_with(6, 8).and_will_return(7).when(function() new_val = rand() end)
         assert.are.equal(19, new_val)
 
-        f:should_be_called_with(6, 8):and_will_return(7):when(function() new_val = rand() end)
+        f.should_be_called_with(6, 8).and_will_return(7).when(function() new_val = rand() end)
         assert.are.equal(14, new_val)
     end)
 end)
