3a98e1fcd16748497632ef0bc57afb3f9f9296fa
sam <samdup123@gmail.com>
2019-03-20 13:33:27 -0400

    added more triangles

--- menu_engine/src/presenters/game.lua
+++ menu_engine/src/presenters/game.lua
@@ -5,7 +5,7 @@ local game_rectangle = {
     x = 10,
     y = 10,
     width = 1100,
-    height = 700,
+    height = 875,
 
     red = 255,
     green = 255,
@@ -17,7 +17,7 @@ local console_rectangle = {
     x = game_rectangle.x + game_rectangle.width + 10,
     y = game_rectangle.y,
     width = 650,
-    height = 700,
+    height = game_rectangle.height,
 
     red = 255,
     green = 255,
@@ -38,7 +38,7 @@ local button = function(x,y)
     }
 end
 
-local play_back_button = button(game_rectangle.x + 400, game_rectangle.y + game_rectangle.height + 30)
+local play_back_button = button(game_rectangle.x + 500, game_rectangle.y + game_rectangle.height + 30)
 local step_back_button = button(play_back_button.x + play_back_button.width + 30, play_back_button.y)
 local pause_button = button(step_back_button.x + step_back_button.width + 30, play_back_button.y)
 local compile_button = button(pause_button.x + pause_button.width + 30, play_back_button.y)
@@ -64,10 +64,36 @@ local triangle = function(top_corner, height, width, direction, white)
     }
 end
 
-local play_back_triangle_1 = triangle({x = 50, y = 50}, 30, 30, -1, false)
-local play_back_triangle_2 = triangle({x = 62.5, y = 50}, 30, 30, -1, true)
-local play_back_triangle_3 = triangle({x = 72.5, y = 50}, 30, 30, -1, false)
-local play_back_triangle_4 = triangle({x = 85, y = 50}, 30, 30, -1, true)
+local function center_x(area) return area.x + area.width/2 end
+local function center_y(area) return area.y + area.height/2 end
+
+local height_of_triangles = 30
+local width_of_triangles = 30
+local center_x_pb_button = center_x(play_back_button)
+local center_y_pb_button = center_y(play_back_button) - height_of_triangles/2
+
+local play_back_triangle_1 = triangle({x = center_x_pb_button, y = center_y_pb_button}, width_of_triangles, height_of_triangles, -1, false)
+local play_back_triangle_2 = triangle({x = center_x_pb_button + 12.5, y = center_y_pb_button}, width_of_triangles, height_of_triangles, -1, true)
+local play_back_triangle_3 = triangle({x = center_x_pb_button + 22.5, y = center_y_pb_button}, width_of_triangles, height_of_triangles, -1, false)
+local play_back_triangle_4 = triangle({x = center_x_pb_button + 35, y = center_y_pb_button}, width_of_triangles, height_of_triangles, -1, true)
+
+local center_x_p_button = center_x(play_button) - 32.5
+local center_y_p_button = center_y(play_button) - height_of_triangles/2
+
+local play_triangle_1 = triangle({x = center_x_p_button, y = center_y_p_button}, width_of_triangles, height_of_triangles, 1, true)
+local play_triangle_2 = triangle({x = center_x_p_button + 12.5, y = center_y_p_button}, width_of_triangles, height_of_triangles, 1, false)
+local play_triangle_3 = triangle({x = center_x_p_button + 22.5, y = center_y_p_button}, width_of_triangles, height_of_triangles, 1, true)
+local play_triangle_4 = triangle({x = center_x_p_button + 35, y = center_y_p_button}, width_of_triangles, height_of_triangles, 1, false)
+
+local center_x_sb_button = center_x(step_back_button) + width_of_triangles/2
+local center_y_sb_button = center_y(step_back_button) - height_of_triangles/2
+
+local step_back_triangle = triangle({x = center_x_sb_button, y = center_y_sb_button}, width_of_triangles, height_of_triangles, -1, false)
+
+local center_x_s_button = center_x(step_button) - width_of_triangles/2
+local center_y_s_button = center_y(step_button) - height_of_triangles/2
+
+local step_triangle = triangle({x = center_x_s_button, y = center_y_s_button}, width_of_triangles, height_of_triangles, 1, false)
 
 return function(release_event, datamodel)
 
@@ -85,11 +111,17 @@ return function(release_event, datamodel)
             pause_button,
             compile_button,
             step_button,
-            play_back_button,
+            play_button,
             play_back_triangle_1,
             play_back_triangle_2,
             play_back_triangle_3,
-            play_back_triangle_4
+            play_back_triangle_4,
+            play_triangle_4,
+            play_triangle_3,
+            play_triangle_2,
+            play_triangle_1,
+            step_back_triangle,
+            step_triangle
         }}
     end,
     click_occurred = function(click)
