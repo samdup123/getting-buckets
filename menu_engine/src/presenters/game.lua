bd5b5378fc7d40bdd45c3d41b711dbcb6b747842
sam <samdup123@gmail.com>
2019-03-20 13:47:49 -0400

    added pause button

--- menu_engine/src/presenters/game.lua
+++ menu_engine/src/presenters/game.lua
@@ -64,36 +64,52 @@ local triangle = function(top_corner, height, width, direction, white)
     }
 end
 
+local height_of_icons = 30
+local width_of_icons = 30
+local pause_button_rectangle_1 = {
+    mode = 'fill',
+    x = pause_button.x + (pause_button.width/2) - (width_of_icons/2),
+    y = pause_button.y + (pause_button.height/2) - (height_of_icons/2),
+    width = width_of_icons/3,
+    height = height_of_icons,
+}
+
+local pause_button_rectangle_2 = {
+    mode = 'fill',
+    x = pause_button_rectangle_1.x + pause_button_rectangle_1.width + (width_of_icons/3),
+    y = pause_button_rectangle_1.y,
+    width = width_of_icons / 3,
+    height = height_of_icons,
+}
+
 local function center_x(area) return area.x + area.width/2 end
 local function center_y(area) return area.y + area.height/2 end
 
-local height_of_triangles = 30
-local width_of_triangles = 30
 local center_x_pb_button = center_x(play_back_button)
-local center_y_pb_button = center_y(play_back_button) - height_of_triangles/2
+local center_y_pb_button = center_y(play_back_button) - height_of_icons/2
 
-local play_back_triangle_1 = triangle({x = center_x_pb_button, y = center_y_pb_button}, width_of_triangles, height_of_triangles, -1, false)
-local play_back_triangle_2 = triangle({x = center_x_pb_button + 12.5, y = center_y_pb_button}, width_of_triangles, height_of_triangles, -1, true)
-local play_back_triangle_3 = triangle({x = center_x_pb_button + 22.5, y = center_y_pb_button}, width_of_triangles, height_of_triangles, -1, false)
-local play_back_triangle_4 = triangle({x = center_x_pb_button + 35, y = center_y_pb_button}, width_of_triangles, height_of_triangles, -1, true)
+local play_back_triangle_1 = triangle({x = center_x_pb_button, y = center_y_pb_button}, width_of_icons, height_of_icons, -1, false)
+local play_back_triangle_2 = triangle({x = center_x_pb_button + 12.5, y = center_y_pb_button}, width_of_icons, height_of_icons, -1, true)
+local play_back_triangle_3 = triangle({x = center_x_pb_button + 22.5, y = center_y_pb_button}, width_of_icons, height_of_icons, -1, false)
+local play_back_triangle_4 = triangle({x = center_x_pb_button + 35, y = center_y_pb_button}, width_of_icons, height_of_icons, -1, true)
 
 local center_x_p_button = center_x(play_button) - 32.5
-local center_y_p_button = center_y(play_button) - height_of_triangles/2
+local center_y_p_button = center_y(play_button) - height_of_icons/2
 
-local play_triangle_1 = triangle({x = center_x_p_button, y = center_y_p_button}, width_of_triangles, height_of_triangles, 1, true)
-local play_triangle_2 = triangle({x = center_x_p_button + 12.5, y = center_y_p_button}, width_of_triangles, height_of_triangles, 1, false)
-local play_triangle_3 = triangle({x = center_x_p_button + 22.5, y = center_y_p_button}, width_of_triangles, height_of_triangles, 1, true)
-local play_triangle_4 = triangle({x = center_x_p_button + 35, y = center_y_p_button}, width_of_triangles, height_of_triangles, 1, false)
+local play_triangle_1 = triangle({x = center_x_p_button, y = center_y_p_button}, width_of_icons, height_of_icons, 1, true)
+local play_triangle_2 = triangle({x = center_x_p_button + 12.5, y = center_y_p_button}, width_of_icons, height_of_icons, 1, false)
+local play_triangle_3 = triangle({x = center_x_p_button + 22.5, y = center_y_p_button}, width_of_icons, height_of_icons, 1, true)
+local play_triangle_4 = triangle({x = center_x_p_button + 35, y = center_y_p_button}, width_of_icons, height_of_icons, 1, false)
 
-local center_x_sb_button = center_x(step_back_button) + width_of_triangles/2
-local center_y_sb_button = center_y(step_back_button) - height_of_triangles/2
+local center_x_sb_button = center_x(step_back_button) + width_of_icons/2
+local center_y_sb_button = center_y(step_back_button) - height_of_icons/2
 
-local step_back_triangle = triangle({x = center_x_sb_button, y = center_y_sb_button}, width_of_triangles, height_of_triangles, -1, false)
+local step_back_triangle = triangle({x = center_x_sb_button, y = center_y_sb_button}, width_of_icons, height_of_icons, -1, false)
 
-local center_x_s_button = center_x(step_button) - width_of_triangles/2
-local center_y_s_button = center_y(step_button) - height_of_triangles/2
+local center_x_s_button = center_x(step_button) - width_of_icons/2
+local center_y_s_button = center_y(step_button) - height_of_icons/2
 
-local step_triangle = triangle({x = center_x_s_button, y = center_y_s_button}, width_of_triangles, height_of_triangles, 1, false)
+local step_triangle = triangle({x = center_x_s_button, y = center_y_s_button}, width_of_icons, height_of_icons, 1, false)
 
 return function(release_event, datamodel)
 
@@ -112,6 +128,8 @@ return function(release_event, datamodel)
             compile_button,
             step_button,
             play_button,
+            pause_button_rectangle_1,
+            pause_button_rectangle_2,
             play_back_triangle_1,
             play_back_triangle_2,
             play_back_triangle_3,
