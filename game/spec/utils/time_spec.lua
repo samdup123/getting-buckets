d23f372b1a4f9c821272dcd9783532db189cd656
sam <samdup123@gmail.com>
2019-03-24 16:18:34 -0400

    timers are better

--- game/spec/utils/time_spec.lua
+++ game/spec/utils/time_spec.lua
@@ -4,6 +4,7 @@ describe('time', function()
 
     local f = mach.mock_function('f')
     local g = mach.mock_function('g')
+    local h = mach.mock_function('h')
 
     local time = 0
     local function after(new_time)
@@ -18,63 +19,101 @@ describe('time', function()
         time = 0
     end)
 
-    -- it('should allow a one time timer to be created', function()
-    --     local timer = Time.timer_dispenser()
-    --     timer.one_time(8, f)
-    --
-    --     nothing_should_happen_when(
-    --         function() after(7) end
-    --     )
-    --
-    --     f.should_be_called()
-    --     .when(
-    --         function() after(1) end
-    --     )
-    -- end)
-    --
-    -- it('should allow multiple one time timers to be created (out of order)', function()
-    --     local timer = Time.timer_dispenser()
-    --     timer.one_time(8, f)
-    --     timer.one_time(5, g)
-    --
-    --     nothing_should_happen_when(
-    --         function() after(4) end
-    --     )
-    --
-    --     g.should_be_called()
-    --     .when(function() after(1) end)
-    --
-    --     nothing_should_happen_when(
-    --         function() after(2) end
-    --     )
-    --
-    --     f.should_be_called()
-    --     .when(function() after(1) end)
-    -- end)
+    it('should allow a one time timer to be created', function()
+        local timer = Time.timer_dispenser()
+        timer.one_time(8, f)
+
+        nothing_should_happen_when(function() after(7) end)
+
+        f.should_be_called()
+        .when(function() after(1) end)
+    end)
+
+    it('should allow multiple one time timers to be created (out of order)', function()
+        local timer = Time.timer_dispenser()
+        timer.one_time(8, f)
+        timer.one_time(5, g)
+
+        nothing_should_happen_when(
+            function() after(4) end
+        )
+
+        g.should_be_called()
+        .when(function() after(1) end)
+
+        nothing_should_happen_when(
+            function() after(2) end
+        )
+
+        f.should_be_called()
+        .when(function() after(1) end)
+    end)
 
     it('should allow a repeating timer to be created', function()
         local timer = Time.timer_dispenser()
         timer.repeating(8, f)
 
-        nothing_should_happen_when(
-            function() after(7) end
-        )
+        nothing_should_happen_when(function() after(7) end)
 
         f.should_be_called()
         .when(function() after(1) end)
 
-        nothing_should_happen_when(
-            function() after(7) end
-        )
+        nothing_should_happen_when(function() after(7) end)
 
         f.should_be_called()
         .when(function() after(1) end)
-        --
-        -- nothing_should_happen_when(
-        --     function() after(7) end
-        -- )
-        --
-        -- f.should_be_called()
-        -- .when(function() after(1) end)
+
+        nothing_should_happen_when(function() after(7) end)
+
+        f.should_be_called()
+        .when(function() after(1) end)
+    end)
+
+    it('should allow one time timers to be executed at once', function()
+        local timer = Time.timer_dispenser()
+        timer.one_time(8, f)
+        timer.one_time(8, g)
+
+        nothing_should_happen_when(function() after(7) end)
+
+        f.should_be_called()
+        .and_also(g.should_be_called())
+        .when(function() after(1) end)
+    end)
+
+    it('should allow one time and repeating timers to be executed at once', function()
+        local timer = Time.timer_dispenser()
+        timer.one_time(16, f)
+        timer.repeating(8, g)
+        timer.repeating(8, h)
+
+        nothing_should_happen_when(function() after(7) end)
+
+        g.should_be_called()
+        .and_also(h.should_be_called())
+        .when(function() after(1) end)
+
+        nothing_should_happen_when(function() after(7) end)
+
+        f.should_be_called()
+        .and_also(g.should_be_called())
+        .and_also(h.should_be_called())
+        .when(function() after(1) end)
+
+        nothing_should_happen_when(function() after(7) end)
+
+        g.should_be_called()
+        .and_also(h.should_be_called())
+        .when(function() after(1) end)
+    end)
+
+    it('should still have working one time timers when time happens irregularly', function()
+        local timer = Time.timer_dispenser()
+        timer.one_time(8, f)
+        timer.one_time(9, g)
+
+        f.should_be_called()
+        .and_also(g.should_be_called())
+        .when(function() after(10) end)
     end)
 end)
