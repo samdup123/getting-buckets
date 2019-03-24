1cfe0a5ab9cd4af0c270147bbebab12ed74f3cfb
sam <samdup123@gmail.com>
2019-03-24 17:47:38 -0400

    game plays

--- game/src/menu/presenters/game.lua
+++ game/src/menu/presenters/game.lua
@@ -161,34 +161,34 @@ local bucket_rect = {
 }
 
 local number_of_chutes
+local ball_drop_ammount
 local balls = {}
 local history
-local time_between_frames = .4
+local time_between_frames = .15
 local current_timer_token
 local current_fame = 0
 
 local datamodel_on_change_callback = function(label, data)
     if label == 'current level environment' then
         number_of_chutes = data.chutes.info().number_of_chutes
-        
+        local length_of_chutes = data.chutes.info().length_of_chutes
 
-        local height_of_bucket = chutes_rect.height /
-        local width_of_bucket = chutes_rect.width / number_of_chutes
-        bucket_rect.width = width_of_bucket
-        bucket_rect.height = width_of_bucket
+        local width_of_balls = chutes_rect.width / number_of_chutes
 
-        chutes_rect.height = game_rect.y + game_rect.height - 40 - width_of_bucket
+        bucket_rect.width = width_of_balls
+        bucket_rect.height = width_of_balls
+
+        chutes_rect.height = game_rect.y + game_rect.height - 40 - width_of_balls
         bucket_rect.y = chutes_rect.y + chutes_rect.height
+
+        ball_drop_ammount = chutes_rect.height / length_of_chutes
     elseif label == 'current game history' then
-        print('the current game history just came in through the on change event', #data)
         history = data
     end
 end
 
 local function update_game_frame()
-    print('updating game frame')
     if history then
-        print('in here doing it')
         current_fame = current_fame + 1
         balls = {}
         for _,ball in ipairs(history[current_fame].balls_in_play) do
@@ -197,13 +197,11 @@ local function update_game_frame()
                     mode = 'fill',
                     r = bucket_rect.width / 2,
                     x = (ball.chute - .5) * bucket_rect.width + chutes_rect.x,
-                    y = (ball.location - .5) * bucket_rect.height + chutes_rect.y,
+                    y = (ball.location - .5) * ball_drop_ammount + chutes_rect.y,
                     red = 255, green = 255, blue = 255
                 }
             )
         end
-        print('#balls', #balls)
-
         bucket_rect.x = (history[current_fame].bucket_position - 1) * bucket_rect.width + chutes_rect.x
     end
 end
